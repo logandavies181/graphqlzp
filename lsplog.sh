1a9ec62279cb7f854033e58d752265f3a88015ca
logandavies181 <logan.davies181@gmail.com>
2025-05-30 20:29:09 +1200

    minor comment for posterity

--- lsplog.sh
+++ lsplog.sh
@@ -4,6 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Hackily read the last $1 lines from the LSP log, which is otherwise
+# kinda hard to because newlines are escaped.
+
 tail -n ${1:-10} ~/.local/state/nvim/lsp.log |
   awk '/stderr/ {print $2}' FS="\"stderr\"\t" |
 
